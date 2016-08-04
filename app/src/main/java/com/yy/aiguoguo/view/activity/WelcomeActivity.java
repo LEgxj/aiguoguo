dcc01bb9e97efda09912431af49eecb5c8bc4303
yangyan <yangy@hljdiyuan.com>
2016-08-04 15:10:41 +0800

    删除MainActivity,修改welcome布局

--- app/src/main/java/com/yy/aiguoguo/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/yy/aiguoguo/view/activity/WelcomeActivity.java
@@ -1,10 +1,17 @@
 package com.yy.aiguoguo.view.activity;
 
+import android.os.Bundle;
+
+import com.yy.aiguoguo.R;
+
 /**
  * Created by yyan on 2016/8/3 0003.
  */
 public class WelcomeActivity extends BaseActivity{
 
-
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
 }
