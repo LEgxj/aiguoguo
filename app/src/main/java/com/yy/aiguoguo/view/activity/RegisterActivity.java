14091b4b1a0d6a968da2990947afd64f3e416f9d
yangyan <yangy@hljdiyuan.com>
2016-08-04 15:42:52 +0800

    注册按钮点击事件，欢迎界面

--- app/src/main/java/com/yy/aiguoguo/view/activity/RegisterActivity.java
+++ app/src/main/java/com/yy/aiguoguo/view/activity/RegisterActivity.java
@@ -1,11 +1,12 @@
 package com.yy.aiguoguo.view.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.yy.aiguoguo.R;
 
 /**
- * Created by Administrator on 2016/8/4 0004.
+ * Created by yyan on 2016/8/4 0004.
  */
 public class RegisterActivity extends  BaseActivity{
 
@@ -13,5 +14,8 @@ public class RegisterActivity extends  BaseActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void register(View view){
+        toast.showToast("正在注册。。。");
     }
 }
--- app/src/main/java/com/yy/aiguoguo/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/yy/aiguoguo/view/activity/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.yy.aiguoguo.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.yy.aiguoguo.R;
@@ -8,10 +9,28 @@ import com.yy.aiguoguo.R;
  * Created by yyan on 2016/8/3 0003.
  */
 public class WelcomeActivity extends BaseActivity{
-
+    private static final int sleepTime = 2000;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(getBaseContext(),LoginActivity.class));
+                finish();
+            }
+        }).start();
     }
 }
